deb6a2b931f2655a01f3e95014647d94f3b07a45
Magnus Thomsen <magnus@thomsen.one>
2025-11-17 14:20:18 +0100

    Created Modal atom and organism to display custom errors in error store

---
+++ src/components/atoms/Modal.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import AppButton from '@/components/atoms/Button.vue'
+const props = defineProps<{
+  open: boolean
+}>()
+
+const emit = defineEmits<{
+  (e: 'close'): void
+}>()
+
+function closeModal() {
+  emit('close')
+}
+</script>
+
+<template>
+  <div
+    class="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
+    v-if="open"
+    @click.self="closeModal"
+  >
+    <div
+      class="bg-white rounded-xl shadow-xl p-6 w-full max-w-md"
+      @keydown.esc="closeModal"
+      tabindex="0"
+    >
+      <slot></slot>
+      <AppButton type="primary" size="md" class="mt-4 w-full" @click="closeModal">
+        Close
+      </AppButton>
+    </div>
+  </div>
+</template>
---
+++ src/components/organisms/ErrorModal.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Modal from '@/components/atoms/Modal.vue'
+import { useErrorStore } from '@/stores/error'
+
+const errorStore = useErrorStore()
+</script>
+
+<template>
+  <Modal :open="errorStore.show" @close="errorStore.clearError()">
+    <h2 class="text-xl font-bold mb-4">Error</h2>
+    <p class="text-base text-gray-700">{{ errorStore.message }}</p>
+  </Modal>
+</template>
