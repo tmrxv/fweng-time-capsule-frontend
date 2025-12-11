ce4a83651162da6ad73ddc5dc2f973e9624b58f8
Magnus Thomsen <magnus@thomsen.one>
2025-12-11 19:38:11 +0100

    fix: allows errormodal to accept array and string

--- src/components/organisms/ErrorModal.vue
+++ src/components/organisms/ErrorModal.vue
@@ -8,6 +8,17 @@ const errorStore = useErrorStore()
 <template>
   <Modal :open="errorStore.show" @close="errorStore.clearError()">
     <h2 class="text-xl font-bold mb-4">Error</h2>
-    <p class="text-base text-gray-700">{{ errorStore.message }}</p>
+    <p
+      v-if="typeof errorStore.message === 'string'"
+      class="text-base text-gray-700 whitespace-pre-line"
+    >
+      {{ errorStore.message }}
+    </p>
+
+    <ul v-else class="list-disc pl-5 space-y-1 text-base text-gray-700">
+      <li v-for="(msg, i) in errorStore.message.filter((m) => m)" :key="i">
+        {{ msg[0].toUpperCase() + msg.slice(1) }}
+      </li>
+    </ul>
   </Modal>
 </template>
